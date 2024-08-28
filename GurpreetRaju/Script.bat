dda889a3c54d75fb4c11ebe4a0ced0b78a08ad5b
Gurpreet Raju <gurpreetraj92@gmail.com>
2024-08-28 00:12:29 -0400

    update batch file

--- GurpreetRaju/Script.bat
+++ GurpreetRaju/Script.bat
@@ -1,4 +1,17 @@
-git clean
-git checkout -f "gh-pages"
-xcopy /e . "gurpreetraju.github.io"
+echo Starting...
+cd ../gurpreetraju.github.io
 
+echo Running GIT Clean
+git clean -f
+
+echo Running GIT Checkout
+git checkout -f "gh-pages"
+
+echo Checkout complete
+cd ../Temporary
+
+echo Start copying files...
+xcopy .\wwwroot\ ..\gurpreetraju.github.io\ /e /y
+
+echo Complete
+pause
